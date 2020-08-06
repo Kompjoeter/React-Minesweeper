2eacd5ac8b54acf466624601d4e32a023cb1160a
RanDByyp <randbyyp@gmail.com>
2020-08-06 23:56:58 +0200

    Updates

---
+++ precache-manifest.8806fb2bc66e6eaf8137e4548c17aa45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4a7e08e1317643427a9405dec7f03c",
+    "url": "/RanDByyp/React-Minesweeper/index.html"
+  },
+  {
+    "revision": "9fed600746d7c7749ffd",
+    "url": "/RanDByyp/React-Minesweeper/static/css/main.85fca585.chunk.css"
+  },
+  {
+    "revision": "a14364b9c580d4b8905b",
+    "url": "/RanDByyp/React-Minesweeper/static/js/2.b22823cb.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/RanDByyp/React-Minesweeper/static/js/2.b22823cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fed600746d7c7749ffd",
+    "url": "/RanDByyp/React-Minesweeper/static/js/main.8d62e616.chunk.js"
+  },
+  {
+    "revision": "4420f9437a9025d65837",
+    "url": "/RanDByyp/React-Minesweeper/static/js/runtime-main.78203a9d.js"
+  }
+]);
